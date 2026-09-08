256f8fd0b0d655769602e37876dccc4a0a64d150
Jeric Jiang <jian1105@purdue.edu>
2026-09-08 13:31:23 -0400

    Lab 2

--- CGT-215-Labs/CGT-215-Lab-02-jian1105.cpp
+++ CGT-215-Labs/CGT-215-Lab-02-jian1105.cpp
@@ -1,7 +1,20 @@
-//#include <iostream>
-//
-//using namespace std;
-//
-//int main() {
-//	float 
-//}
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	float a, b;
+	cout << "ax + b = 0" << endl << "for x" << endl;
+
+	cout << "Please enter a value for a: ";
+	cin >> a;
+
+	cout << "Please enter a value for b: ";
+	cin >> b;
+
+	cout << "Solving " << a << "x + " << b << " = 0 for x..." << endl;
+
+	float x = -b / a;
+
+	cout << "The answer is:" << endl << "x = " << x;
+}
